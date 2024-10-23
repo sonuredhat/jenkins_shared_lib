68f602e78d420e8989c158fb0ab28c70782ee83f
vyanktesh prasad <94549611+sonuredhat@users.noreply.github.com>
2024-10-23 05:42:23 +0530

    Create uploadArtifactJfrog.groovy

---
+++ vars/uploadArtifactJfrog.groovy
@@ -0,0 +1,26 @@
+def call(artifactPath, jarFile, authToken) {
+    def url = new URL(artifactPath)
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection()
+
+    // Set request properties
+    connection.setRequestMethod("PUT")
+    connection.setRequestProperty("Authorization", "Bearer ${authToken}")
+    connection.setDoOutput(true)
+
+    // Upload the file
+    def file = new File(jarFile)
+    connection.outputStream.withCloseable { out ->
+        out.write(file.bytes)
+    }
+
+    // Get the response
+    def responseCode = connection.getResponseCode()
+    def responseMessage = connection.getResponseMessage()
+
+    if (responseCode == HttpURLConnection.HTTP_OK || responseCode == HttpURLConnection.HTTP_CREATED) {
+        println("Artifact uploaded successfully: ${responseMessage}")
+    } else {
+        println("Failed to upload artifact. Response code: ${responseCode}, Message: ${responseMessage}")
+    }
+}
+
